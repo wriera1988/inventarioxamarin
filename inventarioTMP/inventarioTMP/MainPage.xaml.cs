ea4401d412967d80c82704576b43637b0b170221
Alejandro Riera <e1722773262@uisrael.edu.ec>
2021-11-02 12:45:42 -0500

    Importacion inicial, login tempoal, menu

---
+++ inventarioTMP/inventarioTMP/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+using Xamarin.Forms;
+
+namespace inventarioTMP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            var usuario = txtUsuario.Text;
+            var contrasena = txtContrasena.Text;
+            if (String.IsNullOrEmpty(usuario) || String.IsNullOrEmpty(contrasena))
+            {
+                DisplayAlert("No permitido.!", "Ingrese sus credenciales", "Ok");
+            }
+            else if (usuario.Equals("Admin") && contrasena.Equals("Admin"))
+            {
+                Navigation.PushAsync(new MenuP(), false);
+            }
+            else {
+                DisplayAlert("No permitido.!", "Credenciales de ingreso inválidas", "Ok");
+            }
+
+
+        }
+    }
+}
